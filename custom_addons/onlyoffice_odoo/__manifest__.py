e1753b22753374a1c84492bf5497ed24c0e543a0
zzafercakar <zzafercakar@gmail.com>
2025-12-22 15:57:16 +0300

    chore(onlyoffice): force attach icon visibility with css and bump to 5.3.2

--- custom_addons/onlyoffice_odoo/__manifest__.py
+++ custom_addons/onlyoffice_odoo/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "ONLYOFFICE",
     "website": "https://github.com/ONLYOFFICE/onlyoffice_odoo",
     "category": "Productivity",
-    "version": "5.3.1",
+    "version": "5.3.2",
     "depends": ["base", "mail"],
     "external_dependencies": {"python": ["pyjwt"]},
     # always loaded
