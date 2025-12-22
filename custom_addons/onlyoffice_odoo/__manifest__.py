9800f62585336c5aeb954d39ac1f7e0e56b60338
zzafercakar <zzafercakar@gmail.com>
2025-12-22 16:23:13 +0300

    chore(onlyoffice): bump to 5.3.3 and refresh chatter asset log

--- custom_addons/onlyoffice_odoo/__manifest__.py
+++ custom_addons/onlyoffice_odoo/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "ONLYOFFICE",
     "website": "https://github.com/ONLYOFFICE/onlyoffice_odoo",
     "category": "Productivity",
-    "version": "5.3.2",
+    "version": "5.3.3",
     "depends": ["base", "mail"],
     "external_dependencies": {"python": ["pyjwt"]},
     # always loaded
